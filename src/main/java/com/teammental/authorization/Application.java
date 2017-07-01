732c45342b3b141d9742632773b660eab2566ba1
Okan Demirtaş <demirtasokan@gmail.com>
2017-07-01 13:36:12 +0300

    Springboot projesine Thymeleaf config eklendi.

---
+++ src/main/java/com/teammental/authorization/Application.java
@@ -0,0 +1,17 @@
+package com.teammental.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by okan on 1.07.2017.
+ */
+
+@SpringBootApplication
+public class Application extends WebMvcConfigurerAdapter {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
